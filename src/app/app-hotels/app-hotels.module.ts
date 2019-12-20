4adf29bb635498067cef8d16c63fd511801c4fc7
R.Kuznetsov <roman.kuznetsov@g5e.com>
2019-12-20 12:59:15 +0200

    HomeWork-2 Added teddy-bear component

--- src/app/app-hotels/app-hotels.module.ts
+++ src/app/app-hotels/app-hotels.module.ts
@@ -6,10 +6,13 @@ import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ListComponent } from './list/list.component';
+import { VillageComponent } from './village/village.component';
+import { TeddyBearComponent } from './teddy-bear/teddy-bear.component';
 
 
 @NgModule({
-  declarations: [AppHotelsComponent, WeatherComponent, ProfileComponent, HeaderComponent, FooterComponent, ListComponent],
+  declarations: [AppHotelsComponent, WeatherComponent, ProfileComponent,
+    HeaderComponent, FooterComponent, ListComponent, VillageComponent, TeddyBearComponent],
   imports: [
     BrowserModule
   ],
