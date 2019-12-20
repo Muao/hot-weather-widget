b045fc31bb96264ee197a24dd5ba17aa2f34790f
R.Kuznetsov <roman.kuznetsov@g5e.com>
2019-12-20 12:27:20 +0200

    HomeWork-2 Added needs empty components

--- src/app/app-hotels/app-hotels.module.ts
+++ src/app/app-hotels/app-hotels.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core';
 import {AppHotelsComponent} from './app-hotels.component';
 import {BrowserModule} from '@angular/platform-browser';
+import { WeatherComponent } from './weather/weather.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
-  declarations: [AppHotelsComponent],
+  declarations: [AppHotelsComponent, WeatherComponent, ProfileComponent, HeaderComponent, FooterComponent, ListComponent],
   imports: [
     BrowserModule
   ],
