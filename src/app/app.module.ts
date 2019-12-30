eae73f22ff29ff7315d403bc081058e8ec42e465
r.kuznetsov <144kuznetsov@gmail.com>
2019-12-30 10:57:35 +0200

    HomeWork-3 Shared Module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './shared/shared.module';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
@@ -8,15 +9,14 @@ import { FooterComponent } from './footer/footer.component';
 import { ListComponent } from './list/list.component';
 import { ListItemComponent } from './list-item/list-item.component';
 import {AppHotelsComponent} from './app-hotels/app-hotels.component';
-import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [AppHotelsComponent, AppComponent, WeatherComponent, ProfileComponent,
     HeaderComponent, FooterComponent, ListComponent, ListItemComponent],
   imports: [
     BrowserModule,
-    MatButtonModule
-
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
