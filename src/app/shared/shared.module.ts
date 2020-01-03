de2f5e1f85e8e441abe59977f7238bfed3580c62
r.kuznetsov <144kuznetsov@gmail.com>
2020-01-03 20:05:11 +0200

    HomeWork-4 Worked with delete button style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
@@ -10,7 +11,8 @@ import {MatButtonModule} from '@angular/material/button';
     CommonModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class SharedModule { }
